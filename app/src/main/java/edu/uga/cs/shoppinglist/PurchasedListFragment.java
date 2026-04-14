e9d6569d60a88ce8230be6589b0716dbba0f7ea5
Alexia Toma <alexia.g.toma.328@gmail.com>
2026-04-14 12:54:07 -0400

    Added base homepage layout and drawer_menu. Added empty fragments for Purchased List and Shopping List to be displayed in ViewPager

---
+++ app/src/main/java/edu/uga/cs/shoppinglist/PurchasedListFragment.java
@@ -0,0 +1,10 @@
+package edu.uga.cs.shoppinglist;
+
+import androidx.fragment.app.Fragment;
+
+public class PurchasedListFragment extends Fragment {
+
+    public PurchasedListFragment() {
+        super(R.layout.fragment_purchased_list);
+    }
+}
---
+++ app/src/main/java/edu/uga/cs/shoppinglist/ShoppingListFragment.java
@@ -0,0 +1,11 @@
+package edu.uga.cs.shoppinglist;
+
+
+import androidx.fragment.app.Fragment;
+
+public class ShoppingListFragment extends Fragment {
+
+    public ShoppingListFragment() {
+        super(R.layout.fragment_shopping_list);
+    }
+}
