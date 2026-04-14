f670b86f0dd1c39ecf2837fe3470709c2aa61f8c
Alexia Toma <alexia.g.toma.328@gmail.com>
2026-04-14 11:15:52 -0400

    Added Firebase Authentication in LogInActivity, Log In successful in log cat with user in firebase

--- app/src/main/java/edu/uga/cs/shoppinglist/LogInActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/LogInActivity.java
@@ -1,20 +1,37 @@
 package edu.uga.cs.shoppinglist;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LogInActivity extends AppCompatActivity {
 
+    public static final String TAG = "LogInActivity";
     EditText loginEmailEditText;
     EditText loginPasswordEditText;
     Button logInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +47,35 @@ public class LogInActivity extends AppCompatActivity {
         loginEmailEditText = findViewById(R.id.loginEmailEditText);
         loginPasswordEditText = findViewById(R.id.loginPasswordEditText);
         logInButton = findViewById(R.id.loginButton);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailText = loginEmailEditText.getText().toString();
+                String passwordText = loginPasswordEditText.getText().toString();
+                mAuth = FirebaseAuth.getInstance();
+
+                mAuth.signInWithEmailAndPassword( emailText, passwordText )
+                        .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d( TAG, "signInWithEmail:success" );
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                }
+                                else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.d( TAG, "signInWithEmail:failure", task.getException() );
+                                    Toast.makeText( LogInActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
+
+
 
 }
